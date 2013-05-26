bdbc4cd5aecf948d89ac688aa5fb7d73ac1cf974
Stephen J. Butler <stephen.butler@gmail.com>
2013-05-26 02:34:30 -0500

    Add the OpenBadges.me badge desiner!

--- js/admin-post.js
+++ js/admin-post.js
@@ -1,8 +1,68 @@
-jQuery(function ($) {
+jQuery(document).ready(function ($) {
   var $descriptiondiv = $('#wpbadger-badge-descriptiondiv');
   if ($descriptiondiv.length > 0) {
     $descriptiondiv.insertBefore('#postdivrich');
     wptitlehint('wpbadger-badge-description');
   }
+
+  var designerOnError = function (response) {
+    alert( response.data.message );
+  };
+
+  var designerOnSuccess = function (response) {
+    if (!response.success) {
+      designerOnError( response );
+      return;
+    }
+
+    $('.inside', '#postimagediv').html( response.data.postimagediv );
+    $.fancybox.close();
+  };
+
+  /* handle fancebox manually via delegated events to handle when the user
+   * removes the feature image */
+  $(document).on( 'click', '#wpbadger-badge-designer', function (evt) {
+    evt.preventDefault();
+
+    var $this = $(this);
+
+    $.fancybox({
+      width: '95%',
+      height: '95%',
+      minHeight: 680,
+      autoSize: false,
+      closeClick: false,
+      openEffect: 'fade',
+      closeEffect: 'none',
+      href: $this.attr( 'href' ),
+      type: 'iframe',
+    });
+  });
+
+  /* handle the OpenBadges.me callback. Post the data to WP via ajax */
+  $(window).on('message', function (evt) {
+    evt = evt.originalEvent;
+
+    if (evt.origin != 'https://www.openbadges.me')
+      return;
+
+    if (evt.data.image == 'cancelled') {
+      $.fancybox.close();
+      return;
+    }
+
+    var $designer = $('#wpbadger-badge-designer');
+    $.post(
+      ajaxurl,
+      {
+        action: 'wpbadger_badge_designer_publish',
+      badge: evt.data,
+      post_id: $designer.data( 'post-id' ),
+      nonce: $designer.data( 'nonce' ),
+      }
+    )
+    .done( designerOnSuccess )
+    .fail( designerOnError );
+  });
 });
 
--- wpbadger.php
+++ wpbadger.php
@@ -55,6 +55,10 @@ function wpbadger_admin_init()
 {
     wp_register_style( 'wpbadger-admin-styles', plugins_url('css/admin-styles.css', __FILE__) );
     wp_register_script( 'wpbadger-admin-post', plugins_url('js/admin-post.js', __FILE__), array( 'post' ) );
+
+    /* fancybox stuff */
+    wp_register_style( 'fancybox-styles', plugins_url('fancybox/jquery.fancybox.css', __FILE__) );
+    wp_register_script( 'fancybox', plugins_url('fancybox/jquery.fancybox.js', __FILE__), array( 'jquery' ) );
 }
 
 function wpbadger_admin_head()
@@ -66,9 +70,13 @@ function wpbadger_admin_head()
         return;
 
     wp_enqueue_style( 'wpbadger-admin-styles' );
+    wp_enqueue_style( 'fancybox-styles' );
 
     if ($pagenow == 'post.php' || $pagenow == 'post-new.php')
+    {
         wp_enqueue_script( 'wpbadger-admin-post' );
+        wp_enqueue_script( 'fancybox' );
+    }
 }
 
 function wpbadger_enqueue_scripts()
